4070287f49803171043e1a64d744bad021b0dd2d
mancinilucas <mancini.lucasg@gmail.com>
2024-08-23 20:00:58 -0300

    fix: corrigido erro de renderização do endpoint

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import status
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Livro
 from .serializers import LivroSerializer
+from rest_framework.decorators import api_view
 
 
 @csrf_exempt
+@api_view(['GET', 'POST'])
 def livro_list_create(request):
     if request.method == 'GET':
         livros = Livro.objects.all()
